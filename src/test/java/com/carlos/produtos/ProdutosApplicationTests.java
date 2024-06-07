1f45c15bf81e7a0dc4b09a60bc72aab1d9f9b852
Carlos Eduardo <carloseduardo.moura.contato@gmail.com>
2024-06-07 19:57:22 -0300

    Criando o projeto

---
+++ src/test/java/com/carlos/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
